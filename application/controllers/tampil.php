f1cd3364627dbf2703bb8a151407523f48a85cb0
fahmimoch22 <fahmimochramdani22@gmail.com>
2022-09-20 15:59:56 +0700

    first commit

---
+++ application/controllers/tampil.php
@@ -0,0 +1,8 @@
+<?php
+class Tampil extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('tampil');
+	}
+}
---
+++ application/views/tampil.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title> Latihan Pertama</title>
+</head>
+<body>
+    <p>Latihan Pertama saya belajar framework codeigniter</p>
+    <p>Framework Codeigniter sangat mudah di pelajari</p>
+    
+</body>
+</html>
